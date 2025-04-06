8e9fb13c425cdc5333a01fe730ce2e86db05a029
Favour Ebuka <favourvanderwaal@gmail.com>
2025-04-06 15:04:03 +0100

    theme variables

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <>
+      <div className="btn-primary">Dashboard</div>
+      <h1>hello</h1>
+    </>
+  );
+};
+
+export default Dashboard;
